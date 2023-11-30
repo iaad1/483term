31d5fa6c981289d5a78338ab62cfd6c7e0ee2497
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-11-29 21:48:57 -0800

    tset

--- main.py
+++ main.py
@@ -3,3 +3,10 @@
 # Shawyan Tabari
 
 # Term 483 Project
+
+# Imports
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
