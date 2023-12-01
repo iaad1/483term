8051207206fccc4f0ae7c35a7bd5c5036e5e193b
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-11-30 17:57:21 -0800

    working on preparing data, need to figure out how to create the is_holiday column

--- main.py
+++ main.py
@@ -9,11 +9,36 @@
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.cluster import KMeans
+from sklearn.ensemble import GradientBoostingRegressor
 
-# Turn all the csvs into dataframes using pandas.
-holidayDf = pd.read_csv('holiday_events.csv')
+
+# STEP 1 (SECURE THE KEYS!)
+# Read all the relevant csvs into the dataframes.
+print('READING CSVS')
+holidayDf = pd.read_csv('holidays_events.csv')
 oilDf = pd.read_csv('oil.csv')
 trainDf = pd.read_csv('train.csv')
 testDf = pd.read_csv('test.csv')
+
+# STEP 2 (ASCEND FROM DARKNESS!)
+# Merge the holiday and oil data onto the training and test frames
+trainDf = pd.merge(trainDf, oilDf, on="date", how="left") # Cute little left join
+trainDf = pd.merge(trainDf, holidayDf, on="date", how="left")
+testDf = pd.merge(testDf, oilDf, on="date", how="left") # Cute little left join
+testDf = pd.merge(testDf, holidayDf, on="date", how="left")
+
+
+# STEP 3 (RAIN FIRE)
+# Prepare the dataset, convert values
+
+# Convert onpromotion to a boolean value
+trainDf['onpromotion'] = (trainDf['onpromotion'] > 0).astype(int)
+testDf['onpromotion'] = (testDf['onpromotion'] > 0).astype(int)
+
+# Change all the holiday junk to a simple is_holiday numerical column.
+trainDf['is_holiday'] = (pd.notna(trainDf['type']) & trainDf['transferred'] != True).astype(int)
+print(trainDf.head(4000))
+
+
