2a7e9b0cf13fdbc335b5cc94219198e7815086de
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-11-30 17:06:21 -0800

    created dataframes for the csvs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Shawyan Tabari
 
 # Term 483 Project
+# Kaggle: https://www.kaggle.com/competitions/store-sales-time-series-forecasting/data
 
 # Imports
 import pandas as pd
@@ -10,3 +11,9 @@ import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
+
+# Turn all the csvs into dataframes using pandas.
+holidayDf = pd.read_csv('holiday_events.csv')
+oilDf = pd.read_csv('oil.csv')
+trainDf = pd.read_csv('train.csv')
+testDf = pd.read_csv('test.csv')
